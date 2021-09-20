5f96d75e41c9d2fb5f5706a1baddd7c17192bcb2
JoshHughes156 <42577765+JoshHughes156@users.noreply.github.com>
2021-09-20 09:22:04 +0000

    Comments and removed test code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from math import log2, ceil
 from copy import deepcopy
 
-def bintodec(bin): # does stuff
+def bintodec(bin): # Converts binary to decimal
 	dec = 0
 	bin = str(bin)
 	for i in range(len(bin)): # Iterate over the binary number
@@ -9,26 +9,15 @@ def bintodec(bin): # does stuff
 			dec += 2**(len(bin)-i)
 	return dec//2 # Have to half it as dec comes out to 2 times the expected value
 
-def dectobin(dec): # does other stuff
-	if dec == 0:
+def dectobin(dec): # Converts decimal to binary
+	if dec == 0: # Edge case
 		return "0"
 	bin = ""
 	dl = deepcopy(dec)
-	for i in range(ceil(log2(dec))+1, -1, -1):
+	for i in range(ceil(log2(dec))+1, -1, -1): # If we can take away a power of 2 (going highest to lowest)
 		if dl - 2**i >= 0:
 			bin += "1"
 			dl -= 2**i
 		else:
 			bin += "0"
 	return bin
-
-
-for i in range(0, 100):
-	print(f"{i}: {bintodec(str(bin(i))[2:])}")
-__import__('sys').exit()
-for i in range(0, 10):
-	#print(f"{str(bin(i))[2:]}: {dectobin(i)}")
-	if int(dectobin(i)) == int(str(bin(i))[2:]):
-		print(f"{i}: Good")
-	else:
-		print(f"{i}: Bad")
