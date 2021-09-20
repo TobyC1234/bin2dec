89cbe451c859528c7c48c4e8a1a94656662d240d
JoshHughes156 <42577765+JoshHughes156@users.noreply.github.com>
2021-09-20 09:11:44 +0000

    Fix bintodec

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from copy import deepcopy
 
 def bintodec(bin): # does stuff
 	dec = 0
-	for i in range(0, len(bin)):
-		dec += int(bin[i]) * 2**i
-	return dec
+	bin = str(bin)
+	for i in range(len(bin)): # Iterate over the binary number
+		if bin[i] == "1": # If the binary number is a one we want to add the corresponding power of 2
+			dec += 2**(len(bin)-i)
+	return dec//2 # Have to half it as dec comes out to 2 times the expected value
 
 def dectobin(dec): # does other stuff
 	if dec == 0:
@@ -21,6 +23,9 @@ def dectobin(dec): # does other stuff
 	return bin
 
 
+for i in range(0, 100):
+	print(f"{i}: {bintodec(str(bin(i))[2:])}")
+__import__('sys').exit()
 for i in range(0, 10):
 	#print(f"{str(bin(i))[2:]}: {dectobin(i)}")
 	if int(dectobin(i)) == int(str(bin(i))[2:]):
